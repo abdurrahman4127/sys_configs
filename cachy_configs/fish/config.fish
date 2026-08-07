9b57d4931da5441950cb972ea4d5584121e432f8
abdurrahman4127 <whoamiiammr.nobody@gmail.com>
2026-08-07 21:10:00 +0600

    alias for pdfdiff --mfking tool btw

--- cachy_configs/fish/config.fish
+++ cachy_configs/fish/config.fish
@@ -21,6 +21,7 @@ alias ff='fastfetch'
 alias cpy='xclip -selection clipboard'
 alias gpu='__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia '
 alias cat='bat'
+alias pdfdiff='diffpdf'
 # -----------------
 
 # --------- to compress pdf
